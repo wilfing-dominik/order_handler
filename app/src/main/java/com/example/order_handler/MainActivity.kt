becb304f10f0cd4f914573c4a91b28346f9acbb5
kamanarmin <91308083+kamanarmin@users.noreply.github.com>
2022-07-05 19:07:24 +0200

    Buttons to mainview

--- app/src/main/java/com/example/order_handler/MainActivity.kt
+++ app/src/main/java/com/example/order_handler/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.order_handler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TableLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var asztal1 = findViewById<Button>(R.id.table1)
+        var table = findViewById<TableLayout>(R.id.tables)
     }
 }
